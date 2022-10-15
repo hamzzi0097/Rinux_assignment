86dafb3510c492c5eabd32ef8733779b63fa6dc3
TaeHyun Kim <81970551+hamzzi0097@users.noreply.github.com>
2022-10-16 02:06:30 +0900

    Add files via upload

---
+++ test1.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# Changes all file names in the current directory to lowercase
+
+cnt=0
+for filename in *
+do
+	fname=$(basename 	$filename)
+	cname=$(echo $fname | tr A-Z a-z)
+	if [ "$fname" !=  "$cname" ] 
+	then
+		if [ -e  "$cname" ]
+		then 
+		   echo  	"$cname already exists"
+		   exit 1
+		fi
+		echo "$fname is renamed $cname"
+		mv $fname $cname
+		cnt=$(( ${cnt}+1 ))
+	fi
+done
+echo "Total count: $cnt"
+exit 0
